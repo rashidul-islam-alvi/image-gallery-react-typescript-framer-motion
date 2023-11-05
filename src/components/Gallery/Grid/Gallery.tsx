f4201651e2f4eaa16f5d65fbcd43e609f5911e9a
Rashidul Islam Alvi <rashidul537@gmail.com>
2023-11-06 02:27:07 +0600

    fixed style again

--- src/components/Gallery/Grid/Gallery.tsx
+++ src/components/Gallery/Grid/Gallery.tsx
@@ -3,7 +3,7 @@ import { GridLayoutGallery } from "./GridLayoutGallery";
 
 export const Gallery = () => {
   return (
-    <div className="relative flex flex-col w-full px-2 md:px-5 ">
+    <div className="relative flex flex-col w-full min-h-screen px-2 md:px-5 ">
       <Header />
       <GridLayoutGallery />
     </div>
